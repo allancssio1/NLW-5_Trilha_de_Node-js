4f46b77d5ed9cd118ce89a9a39cbafd70ad95a78
Allan Freitas <allan.cassio1@gmail.com>
2021-04-20 17:56:14 -0300

    finish routes initial

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
 import express from   "express"
 import "./database"
+import { routes } from "./routes"
+
 const app = express()
 
-app.get("/", (req, res) => {
-  return res.json({
-    message: "NLW#05"
-  })
-})
-app.post("/users", (req, res) => {
-  return res.json({
-    message: "usuário salvo com sucesso"
-  })
-})
+app.use(express.json())
+
+app.use(routes)
 
 app.listen(3333, () => console.log('Server running on port 3333'))
