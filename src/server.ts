b5495cefce7d184dd33efc89bd36d3270525da57
Allan Freitas <allan.cassio1@gmail.com>
2021-04-20 10:12:56 -0300

    conclued class 01

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,15 @@ import express from   "express"
 
 const app = express()
 
+app.get("/", (req, res) => {
+  return res.json({
+    message: "NLW#05"
+  })
+})
+app.post("/users", (req, res) => {
+  return res.json({
+    message: "usuário salvo com sucesso"
+  })
+})
+
 app.listen(3333, () => console.log('Server running on port 3333'))
