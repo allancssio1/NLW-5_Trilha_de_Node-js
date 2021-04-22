af36b5d14bf13f6e0b91762e35ca9416c3d6de1a
Allan Freitas <allan.cassio1@gmail.com>
2021-04-22 15:14:59 -0300

    configured server for to work with public folder

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import express from   "express"
 import { createServer } from "http"
 import { Server, Socket } from "socket.io"
+import path from "path"
 
 import "./database"
 import { routes } from "./routes"
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, "..", "public")))
+app.set("views", path.join(__dirname, "..", "public"))
+app.engine("html", require("ejs").renderFile)
+app.set("view engene", "html")
+
+app.get("/pages/client", (req, res) => {
+  return res.render("html/client.html")
+})
 
 const http = createServer(app) //Criando o protocolo HTTP
 const io = new Server(http) //Criando o protocolo de WebSocket
