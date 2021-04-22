15718f1ab4d6d634f1c98319a310278a6b8ad846
Allan Freitas <allan.cassio1@gmail.com>
2021-04-22 14:57:29 -0300

    updated server.ts for server http and ws

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import express from   "express"
+import { createServer } from "http"
+import { Server, Socket } from "socket.io"
+
 import "./database"
 import { routes } from "./routes"
 
 const app = express()
 
+const http = createServer(app) //Criando o protocolo HTTP
+const io = new Server(http) //Criando o protocolo de WebSocket
+
+io.on("connection", (socket: Socket) => {
+  console.log("Se conectou", socket.id)
+})
+
 app.use(express.json())
 
 app.use(routes)
 
-app.listen(3333, () => console.log('Server running on port 3333'))
+http.listen(3333, () => console.log('Server running on port 3333'))
