8985c99890f154e453b982ba2a6035c43f2d8c03
Allan Freitas <allan.cassio1@gmail.com>
2021-04-23 16:39:42 -0300

    coclued client.ts

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,13 +1,23 @@
 import { io } from "../http"
 import { ConnectionService } from "../services/ConnectionsService"
 import { UsersService } from "../services/UserService"
+import { MessagesService } from "../services/MessagesService"
+
+interface IParams {
+  text: string
+  email: string
+}
 
 io.on("connect", (socket) => {
   const connectionsService = new ConnectionService()
   const usersService = new UsersService()
+  const messagesService = new MessagesService()
+
   socket.on("client_first_access", async (params) => {
     const socket_id = socket.id
-    const { text, email } = params 
+    const { text, email } = params as IParams
+    let user_id = null
+
     const userExists = await usersService.findByEmail(email)
 
     if(!userExists) {
@@ -17,9 +27,12 @@ io.on("connect", (socket) => {
         socket_id,
         user_id: user.id
       })
+      user_id = user.id
     }else {
-      const connection = await connectionsService.findByUserId(userExists.id)
+      user_id = userExists.id
 
+      const connection = await connectionsService.findByUserId(userExists.id)
+      
       if(!connection) {
         await connectionsService.create({
           socket_id,
@@ -30,7 +43,11 @@ io.on("connect", (socket) => {
         
         await connectionsService.create(connection)
       }
-      
     }
+    
+    await messagesService.create({
+      text,
+      user_id
+    }) 
   })
 })
